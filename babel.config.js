abb0cfdd3eaba69f01095380e78530ea850f77d2
mxsgx <45129872+mxsgx@users.noreply.github.com>
2021-09-25 18:41:43 +0700

    Menambahkan module resolver
    - Menambahkan dukungan autocompletion import untuk beberapa editor

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,25 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
+
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          alias: {
+            '@api': './src/api',
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@config': './src/config',
+            '@context': './src/context',
+            '@routes': './src/routes',
+            '@screens': './src/screens',
+            '@utils': './src/utils',
+          },
+        },
+      ],
+    ],
   };
 };
