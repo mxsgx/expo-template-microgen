3dd5b4c3ea84f19f94e6b5ede22f515c5cb4e832
mxsgx <45129872+mxsgx@users.noreply.github.com>
2021-09-25 21:40:21 +0700

    Menambahkan dukungan dotenv

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,18 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          blocklist: null,
+          allowlist: null,
+          safe: false,
+          allowUndefined: false,
+          verbose: false,
+        },
+      ],
+      [
         'module-resolver',
         {
           root: ['.'],
